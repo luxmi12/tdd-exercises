fd8c367c4746ecf4716638fc4a924a7c0c9da13c
Alex Tercete <alextercete@gmail.com>
2017-03-09 16:32:07 +0000

    Generate prime numbers that are less than 1

---
+++ src/Tdd.Exercise1a/PrimeNumbersGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tdd.Exercise1a
+{
+    public static class PrimeNumbersGenerator
+    {
+        public static IEnumerable<int> Generate(uint maxBound)
+        {
+            return Enumerable.Empty<int>();
+        }
+    }
+}
---
+++ tests/Tdd.Exercise1a.Tests/PrimeNumbersGeneratorShould.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Tdd.Exercise1a.Tests
+{
+    [TestFixture]
+    public class PrimeNumbersGeneratorShould
+    {
+        [TestCase((uint)0)]
+        [TestCase((uint)1)]
+        public void ReturnEmptyListWhenMaxBoundIsLessOrEqualTo1(uint maxBound)
+        {
+            var actual = PrimeNumbersGenerator.Generate(maxBound);
+
+            Assert.That(actual, Is.Empty);
+        }
+    }
+}
