61b7ee6e50597776454189d35d517bc8bc2babcc
Alex Tercete <alextercete@gmail.com>
2017-05-26 13:26:28 +0100

    Implement round to make tests pass

--- src/Tdd.Exercise7/Round.cs
+++ src/Tdd.Exercise7/Round.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace Tdd.Exercise7
+﻿namespace Tdd.Exercise7
 {
     public class Round
     {
         public Winner Play(IPlayer player1, IPlayer player2)
         {
-            throw new NotImplementedException();
+            return Winner.Player2;
         }
     }
 }
---
+++ tests/Tdd.Exercise7.Tests/RoundShould.cs
@@ -0,0 +1,33 @@
+﻿
+using NSubstitute;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Tdd.Exercise7.Tests
+{
+    [TestFixture]
+    public class RoundShould
+    {
+        private Round _round;
+        private IPlayer _player1;
+        private IPlayer _player2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _round = new Round();
+            _player1 = Substitute.For<IPlayer>();
+            _player2 = Substitute.For<IPlayer>();
+        }
+
+        [Test]
+        public void Decide_scissors_beats_paper()
+        {
+            _player1.RevealHand().Returns(Hand.Paper);
+            _player2.RevealHand().Returns(Hand.Scissors);
+            var winner = _round.Play(_player1, _player2);
+            winner.ShouldBe(Winner.Player2);
+        }
+    }
+
+}
